155de363277d9141b96996b49dd6e0549c051523
Tung Huynh <htqtung@users.noreply.github.com>
2019-04-23 09:42:51 +0300

    tung

--- src/routes/api/category.js
+++ src/routes/api/category.js
@@ -16,9 +16,15 @@ category.get('/all', function (req, res) {
 /** http://localhost:8787/api/category/    with method=GET **/
 // http://localhost:8787/api/category?id=1 
 category.get('/', function (req, res) {
-  knex.select().from('Category').where('id',req.query.id).then((data) => {
+  console.log(req.query);
+  knex.select().from('Category').where('id', req.query.id).then((data) => {
     res.status(200);
     res.send(data);
+  }
+  ).catch((error) => {
+    console.log('error.errno: ' + error.errno);
+    res.status(400);
+    res.send("Error: " + error.errno);
   });
 });
 
@@ -31,14 +37,14 @@ category.post('/', function (req, res) {
         res.status(200);
         res.send(data);
       }
-    ).catch ((error)=> {
-        if(error.errno == 1062) {  // https://mariadb.com/kb/en/library/mariadb-error-codes/
-          res.status(409);
-          res.send("Conflict: Category with that name already exists!");
-        } else {
-          res.status(400);
-          res.send("Database error, Error number: " +error.errno);
-        }      
+    ).catch((error) => {
+      if (error.errno == 1062) {  // https://mariadb.com/kb/en/library/mariadb-error-codes/
+        res.status(409);
+        res.send("Conflict: Category with that name already exists!");
+      } else {
+        res.status(400);
+        res.send("Database error, Error number: " + error.errno);
+      }
     });
   } else {
     res.status(400);
@@ -50,19 +56,55 @@ category.post('/', function (req, res) {
 
 /** http://localhost:8787/api/category/insertMany    with method=POST **/
 category.post('/insertMany', function (req, res) {
-    knex.insert(req.body).returning('*').into('Category').then(
-      (data) => {
-        res.status(200);
-        res.send(data);
-      }
-    ).catch ((error)=> {
-        if(error.errno == 1062) {  // https://mariadb.com/kb/en/library/mariadb-error-codes/
-          res.status(409);
-          res.send("Conflict: Category with that name already exists!");
-        } else {
-          res.status(400);
-          res.send("Database error, Error number: " +error.errno);
-        }      
+  knex.insert(req.body).returning('*').into('Category').then(
+    (data) => {
+      res.status(200);
+      res.send(data);
+    }
+  ).catch((error) => {
+    if (error.errno == 1062) {  // https://mariadb.com/kb/en/library/mariadb-error-codes/
+      res.status(409);
+      res.send("Conflict: Category with that name already exists!");
+    } else {
+      res.status(400);
+      res.send("Database error, Error number: " + error.errno);
+    }
+  });
+});
+
+category.delete("/deleteOne", (req, res) => {
+  console.log(req);
+  knex("Category")
+    .where("id", req.body.id)
+    .del()
+    .then(() => {
+      res.status(200);
+      res.send("ok");
+    })
+    .catch(e => {
+      res.status(400);
+      res.send("no del");
+    });
+});
+
+category.put("/edit", (req, res) => {
+  const newCategory = {
+    id: req.body.id,
+    name: req.body.name ? req.body.name : "TBA",
+    description: req.body.description ? req.body.description : "TBA",
+    budgetLimit: req.body.budgetLimit ? req.body.budgetLimit : 0,
+    isActive: req.body.isActive ? req.body.isActive : 0
+  };
+  knex("Category")
+    .where("id", req.body.id)
+    .update(newCategory)
+    .then((data) => {
+      res.status(200);
+      res.send("edit ok " + data);
+    })
+    .catch(e => {
+      res.status(400);
+      res.send("no edit");
     });
 });
 
