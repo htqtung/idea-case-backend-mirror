8e5c1476202b6d52d7863ecda0577349d7327423
Tung Huynh <htqtung@users.noreply.github.com>
2019-04-27 00:55:52 +0300

    pull from master

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import express from "express";
-
-import reset_service from "./reset_service";
-import category from "./category";
-import idea from "./idea";
-
-const routes = express.Router();
-
-routes.use("/reset_service", reset_service);
-routes.use("/category", category);
-routes.use("/idea", idea);
-=======
 import express from 'express';
 
 import reset_service from './reset_service';
@@ -27,6 +14,5 @@ routes.use('/comment', comment);
 routes.use('/idea', idea);
 routes.use('/ideaMember', ideaMember);
 routes.use('/member', member);
->>>>>>> 2712544d251fcc02c38f441f75d2b2d26c2bc351
 
 export default routes;
